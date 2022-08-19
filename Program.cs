928a104ca9682b28cd347e7390aec1e5fb9d2353
Ahmed-Imad <ahmedfattal82@gmail.com>
2022-08-20 03:19:10 +0400

    I added 3 methods to make my code a little bit cleaner and I need to do more.

--- Program.cs
+++ Program.cs
@@ -2,64 +2,48 @@
 
 namespace TestingApp 
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+      internal class Program
+     {
+         static void Main(string[] args)
+         {
             // the heading >>>
-
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("--------------------------------------------------------");
-            Console.WriteLine("WELCOME TO THE FUN WIZARD APP!");
-            Console.WriteLine("--------------------------------------------------------");
-            Console.WriteLine();
+            PrintHeader();
             
-            // coloring the text and entering first and last names >>>
+            // coloring the text and getting first and last names >>>
 
-            Console.ForegroundColor= ConsoleColor.White;
-            Console.Write("PLEASE ENTER YOUR FIRST NAME USE ONLY (A-Z) (a-z): ");
-            string? firstName= Console.ReadLine();
-            Console.Write("PLEASE ENTER YOUR LAST NAMEUSE ONLY (A-Z) (a-z): ");
-            string? lastName= Console.ReadLine();
+                Console.ForegroundColor= ConsoleColor.White;
+
 
             //  a new line, printing out first and last name and coloring the text again >>>
-            
-            Console.ForegroundColor= ConsoleColor.Green;
-            Console.WriteLine();
-            Console.WriteLine($"WELCOME MR. {firstName.ToUpper()} {lastName.ToUpper()}.");
+                string fullname = GetUserFullName();
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine(fullname);
+                
                                    
 
             // a new line, entering the first and second numbers for mulit opration >>>
-            try
-            {
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine();
-                Console.Write("PLEASE ENTER FIRST NUMBER? ");
-                string? firstInput = Console.ReadLine();
-                Console.Write("PLEASE ENTER SECOND NUMBER? ");
-                string? secondInput = Console.ReadLine();
-                Console.WriteLine();
-
-
-            // make sure the user is using valid inputs by using Parsing the inputs and then doing the multiplication
-
-         
-                Double intFNumber = Double.Parse(firstInput!);
-                Double intSNumber = Double.Parse(secondInput!);
-                Double total = intFNumber * intSNumber;
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"THE RESULT OF MULTIPLAYING " +
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+                    double total =GetUserNumber();
+                    Console.WriteLine();
+                 
+                // make sure the user is using valid inputs by using Parsing the inputs and then doing the multiplication
+                                          
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"THE RESULT OF MULTIPLAYING " +
                     $"FIRST NUMBER AND SECOND NUMBER IS: {total}");
-                Console.WriteLine();
-            }
-           
-            catch (FormatException)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine($"NO RESULT!");
-                Console.WriteLine($"UNABLE TO PARSE THE FIRST OR SECOND NUMBER!");
-                Console.WriteLine();
-            }
+                    Console.WriteLine();
+                 }
+                  
+                catch (FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"NO RESULT!");
+                    Console.WriteLine($"UNABLE TO PARSE THE FIRST OR SECOND NUMBER!");
+                    Console.WriteLine();
+                }
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("DO YOU LIKE THE APP? ");
@@ -86,7 +70,9 @@ namespace TestingApp
                 case "NOT REALLY":
                     Console.Write("OK! I GOT YOU.");
                     break;
-                    
+                case "exit":
+                    Console.Clear();
+                    break;
                 default: Console.Write("I DON'T UNDERSTAND THAT!");
                     break;
             }
@@ -94,6 +80,39 @@ namespace TestingApp
             Console.ForegroundColor = ConsoleColor.White;
             Console.ReadLine();
         }
+
+        static void PrintHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("WELCOME TO THE FUN WIZARD APP!");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine();
+        }
+        
+        static string GetUserFullName()
+        {
+            Console.Write("PLEASE ENTER YOUR FIRST NAME USE ONLY (A-Z) (a-z): ");
+            string? firstName = Console.ReadLine();
+            Console.Write("PLEASE ENTER YOUR LAST NAMEUSE ONLY (A-Z) (a-z): ");
+            string? lastName = Console.ReadLine();
+            
+            return firstName + " " + lastName;
+        }
+
+        static double GetUserNumber()
+        {
+            Console.Write("PLEASE ENTER FIRST NUMBER? ");
+            string? firstInput = Console.ReadLine();
+            Console.Write("PLEASE ENTER SECOND NUMBER? ");
+            string? secondInput = Console.ReadLine();
+            Double intFNumber = Double.Parse(firstInput!);
+            Double intSNumber = Double.Parse(secondInput!);
+            Double total = intFNumber * intSNumber;
+
+            return total;
+        }
+
 
     }
 }
